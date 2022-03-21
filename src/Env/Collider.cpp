4aad9d156931ef683cf69021699261cb2d372632
Julie Korach <julie.korach@gmail.com>
2022-03-21 19:03:44 +0100

    first commit

---
+++ src/Env/Collider.cpp
@@ -0,0 +1,12 @@
+/*
+ * POOSV EPFL 2022
+ * @authors:
+ * Marco Antognini
+ */
+
+
+#include <Env/Collider.hpp>
+#include <cmath> // abs and fmod
+#include <algorithm>
+
+/* TO BE COMPLETED */
---
+++ src/Env/Collider.hpp
@@ -0,0 +1,19 @@
+/*
+ * POOSV EPFL 2022
+ * @authors: 
+ */
+
+#pragma once
+
+#include <Utility/Vec2d.hpp>
+#include <ostream>
+
+/* TO BE COMPLETED */
+
+class Collider
+{
+
+  /* TO BE COMPLETED */
+
+};
+
